f6f113ff23924ca572201cb3acee833ccdccd03b
unknown <myrzakulovadilet99>
2018-02-06 22:27:53 +0600

    pp2-labs

---
+++ Prime/Prime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            args = s.Split(); // каждое число написать отдельно
+            foreach (string k in args)
+            {
+                int a = int.Parse(k); // переводим стринг на число инт
+                int c = 0;
+                for (int i = 2; i < a; i++)
+                {
+                    if (a % i == 0)
+                        c++;
+                }
+                if (c == 0)
+                    if (c != 1)
+                        Console.WriteLine(a);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+        
